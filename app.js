fde54c42da0cbcf07f8510ff3bbb7d298a44bbed
Alex Pilon <apilo088@gmail.com>
2014-04-29 16:06:27 -0400

    Playing with tabs

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-var app = angular.module('cribster', []);
+var app = angular.module('cribster', ['ui.bootstrap']);
+
+var TabsDemoCtrl = function ($scope) {
+  $scope.tabs = [
+    { title:"Dynamic Title 1", content:"Dynamic content 1" },
+    { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      alert("You've selected the alert tab!");
+    });
+  };
+
+  $scope.navType = 'pills';
+};
+
+app.controller('TabsDemoCtrl', TabsDemoCtrl);
