b1b377496a11a588e11c0829947829bb2b6210b8
Alex Pilon <apilo088@gmail.com>
2014-05-05 19:27:41 -0400

    Remove statusbar

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var app = angular.module('cribster', ['ui.bootstrap','ui.tinymce']);
 
 // Bind controllers
 app.controller('TinyController', function($scope) {
+    //var test = $window.innerWidth;
+    $window.alert("sadsdasf");
     $scope.tinymceOptions = {
         resize: false,
         height: 300,
-        width: 400
+        width: 400,
+        statusbar: false
     };
 });
 
