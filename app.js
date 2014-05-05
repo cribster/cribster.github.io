5f2690a368c085afa959c082e51d0afe909d70d2
Alex Pilon <apilo088@gmail.com>
2014-05-05 19:37:16 -0400

    Fix reference error

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var app = angular.module('cribster', ['ui.bootstrap','ui.tinymce']);
 
 // Bind controllers
 app.controller('TinyController', function($scope) {
-    //var test = $window.innerWidth;
-    $window.alert("sadsdasf");
     $scope.tinymceOptions = {
         resize: false,
         height: 300,
